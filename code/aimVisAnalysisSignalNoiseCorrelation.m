bfc40300415a28b66683b6f6f6f8f9e5ad002ef8
alexianmore1 <68704078+alexianmore1@users.noreply.github.com>
2020-12-07 10:41:27 -0500

    Added plotting for behavior of the hit rate, false alarm rate, and d prime

--- code/aimVisAnalysisSignalNoiseCorrelation.m
+++ code/aimVisAnalysisSignalNoiseCorrelation.m
@@ -93,6 +93,29 @@ lickWindow = 1500;
     
 parsed.contrast = parsed.amplitude;
 
+% hit rate, false alarm rate, and d' calculation 
+smtWin = 50;
+parsed.hitRate = nPointMean(parsed.response_hits, smtWin)';
+parsed.faRate = nPointMean(parsed.response_fa, smtWin)';
+
+figure
+subplot(2,1,1)
+plot(parsed.hitRate);
+ylim([0,1]);
+title('Hit Rate')
+ylabel('P(H)')
+xlabel('Trial Number')
+%legend('Day 4', 'Day 5', 'Day 6')
+hold all
+
+parsed.dPrime = norminv(parsed.hitRate) - norminv(parsed.faRate);
+
+subplot(2,1,2)
+plot(parsed.dPrime);
+title('d Prime')
+xlabel('Trial Number')
+%legend('Day 1', 'Day 2', 'Day 3','Day 4','Day 5');
+ylim([0,2])
 
 % pre/post are ms before/after stimulus onset
 pre = 1000;
